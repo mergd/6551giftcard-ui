e39e6838338f9caca642a302121444eeab59531f
mergd <william.y.xuan@gmail.com>
2023-06-03 11:23:15 -0700

    add things'

---
+++ app/components/footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyFooter() {
+  return (
+    <div className="bg-primary">
+      <div className="div-left">
+        <span>My App &copy; 2021</span>
+      </div>
+      <div className="footer-right">
+        <span>Made with ❤️ by Me</span>
+      </div>
+    </div>
+  );
+}
+
+export default MyFooter;
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,177 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import Navbar from "./components/navbar";
+declare global {
+  interface Window {
+    managegifts: any;
+    opengift: any;
+    creategift: any;
+  }
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className=" ">
+        <Navbar />
+      </div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="card card-compact grid-col bg-base-100 shadow-xl">
+            <figure>
+              <img src="/images/creategift.png" alt="Gift card" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Crypto Gift Cards</h2>
+              <p>
+                Get a crypto gift card for your loved ones, or to get them
+                started with Web3!
+              </p>
+              <div className="card-actions justify-end">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => window.creategift.showModal()}
+                >
+                  Create Gift
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact grid-col bg-base-100 shadow-xl">
+            <figure>
+              <img src="/images/opengifts.png" alt="Open Gift" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Received a code?</h2>
+              <p>
+                Redeem your code here to get started with your web3 journey!
+              </p>
+              <div className="card-actions justify-end">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => window.opengift.showModal()}
+                >
+                  Redeem
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact grid-col bg-base-100 shadow-xl">
+            <figure>
+              <img src="/images/managegifts.png" alt="Manage Gift" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Manage your gifts</h2>
+              <p>Track your gift cards.</p>
+              <div className="card-actions justify-end">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => window.managegifts.showModal()}
+                >
+                  Manage
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <dialog id="managegifts" className="modal modal-bottom sm:modal-middle">
+          <form method="dialog" className="modal-box">
+            <h3 className="font-bold text-lg">Gifts you&apos;ve made</h3>
+            <div className="overflow-x-auto">
+              <table className="table">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Gift Address</th>
+                    <th>Opened</th>
+                    <th>Create Signature</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* row 1 */}
+                  <tr>
+                    <th>1</th>
+                    <td>USDC for Jadid&apos;s birthday</td>
+                    <td>0xsa98...d799a</td>
+                    <td>False</td>
+                    <button className="btn">Sig</button>
+                  </tr>
+                  {/* row 2 */}
+                  <tr>
+                    <th>2</th>
+                    <td>Web3 starter for Wilfred</td>
+                    <td>0xsa98...d799a</td>
+                    <td>True</td>
+                    <button className="btn">Sig</button>
+                  </tr>
+                  {/* row 3 */}
+                  <tr>
+                    <th>3</th>
+                    <td>Jameson&apos;s b-day gift</td>
+                    <td>0xsa98...d799a</td>
+                    <td>True</td>
+                    <button className="btn">Sig</button>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div className="modal-action">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </div>
+          </form>
+        </dialog>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <dialog id="opengift" className="modal modal-bottom sm:modal-middle">
+          <form method="dialog" className="modal-box">
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">
+              <input
+                type="text"
+                placeholder="Enter your code"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </p>
+            <div className="modal-action">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Redeem</button>
+            </div>
+          </form>
+        </dialog>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <dialog id="creategift" className="modal modal-bottom sm:modal-middle">
+          <form method="dialog" className="modal-box w-2/3 h-1/2">
+            <h3 className="font-bold text-lg">Yo!</h3>
+            <p className="py-4">Create gift</p>
+            <div className="modal-action flex-col ">
+              {/* if there is a button in form, it will close the modal */}
+              <p>
+                {" "}
+                Create a gift card first, and then you can fund it
+                <br />
+                <br />
+              </p>
+              <div className="flex  gap-2">
+                <button className="btn">Create</button>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                <button className="btn">Fund</button>
+              </div>
+
+              <input
+                type="text"
+                placeholder="Type who this might be for"
+                className="input w-full max-w-xs"
+              />
+              <br />
+              <button className="btn">Close</button>
+            </div>
+          </form>
+        </dialog>
+      </main>
+    </div>
+  );
 }
